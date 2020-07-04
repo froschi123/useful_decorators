5cfa7dc16194676f694474ba9e0dcbaf1936a349
Bernd Wehmöller <bernd@wehmoeller.net>
2020-07-04 20:13:20 +0200

    bug fixing from 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='useful_decorators',
-    version='1.0.0',
+    version='1.0.2',
     packages=setuptools.find_packages(),
     url='https://github.com/froschi123/useful_decorators',
     license='MIT License',
